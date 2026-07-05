03c293c98b0930e6aeb489c22575f18d1c7a6968
Noe Charmet <noe.charmet@shipfox.io>
2026-07-05 07:57:09 +0200

    Fold workflow evaluation trace migration
    Moves the evaluation_trace column additions into the initial workflows migration and removes the incremental migration artifacts.
    
    Refs ENG-752

--- libs/api/workflows/drizzle/0001_evaluation_trace.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "workflows_job_executions" ADD COLUMN "evaluation_trace" jsonb;--> statement-breakpoint
-ALTER TABLE "workflows_jobs" ADD COLUMN "evaluation_trace" jsonb;--> statement-breakpoint
-ALTER TABLE "workflows_step_attempts" ADD COLUMN "evaluation_trace" jsonb;
