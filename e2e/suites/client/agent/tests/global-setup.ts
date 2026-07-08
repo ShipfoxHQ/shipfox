36550287ed1a33fb5591cb2013304e9b58f2142a
Noe Charmet <noe.charmet@shipfox.io>
2026-07-08 15:49:42 +0200

    Remove Ollama from CI E2E setup

--- e2e/suites/client/agent/tests/global-setup.ts
+++ e2e/suites/client/agent/tests/global-setup.ts
@@ -1,7 +1,5 @@
 import globalSetup from '@shipfox/e2e-kit/global-setup';
-import {requireOllamaModel} from '@shipfox/e2e-setup-agent';
 
 export default async function agentGlobalSetup(): Promise<void> {
   await globalSetup();
-  await requireOllamaModel();
 }
